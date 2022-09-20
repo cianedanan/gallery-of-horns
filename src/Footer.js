8ce1dec0b716ee0ff3314baf8035640ce4121dbd
Cianenry Danan <cianenryd@gmail.com>
2022-09-19 20:16:29 -0700

    Finished Lab.

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+	render(){
+		return(
+			<>
+			<footer> &copy; 2022 Cianenry Danan</footer>
+			</>
+		)
+	}
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import HornedBeast from './HornedBeast.js'
+import weedle from './Weedle.png'
+import rapidash from './Rapidash.png'
+
+class Main extends React.Component {
+	render(){
+		return(
+			<>
+				<div>
+					<HornedBeast title={'Weedle'} src={weedle} alt={'an image of weedle'} p={'A wild Weedle appeared!'}/>
+					<p></p>
+				</div>
+				<div>
+					<HornedBeast title={'Rapidash'}  src={rapidash} alt={'an image of rapidash'} p={'A wild Rapidash appeared!'}/>
+				</div>
+			</>
+		)
+	}
+}
+
+export default Main;
